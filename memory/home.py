e3b4993fe28daaeca3d473da9f7923bd4f324951
Junpei Tsuji <junpei.tsuji.ams@gmail.com>
2024-03-01 18:40:25 +0900

    feat: added memory sample

---
+++ memory/home.py
@@ -0,0 +1,57 @@
+import streamlit as st
+from langchain_openai import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import MessagesPlaceholder, ChatPromptTemplate
+from langchain.chains import ConversationChain
+
+st.title("OpenAI Chat")
+
+system_prompt = """
+あなたはAIアシスタントです。
+"""
+
+def create_chain():
+    chat = ChatOpenAI(
+        model_name="gpt-3.5-turbo",
+    )
+    # OpenAI Functions AgentのプロンプトにMemoryの会話履歴を追加するための設定
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder(variable_name="history"),
+            ("user", "{input}"),
+        ]
+    )
+
+    # OpenAI Functions Agentが使える設定でMemoryを初期化
+    memory = ConversationBufferMemory(memory_key="history", return_messages=True)
+    chain = ConversationChain(
+      llm=chat,
+      memory=memory,
+    )
+
+    return chain
+
+# 一度だけ初期化。セッションで保持しないと毎回初期化されてしまう
+if "chain" not in st.session_state:
+    st.session_state.chain = create_chain()
+
+# 会話履歴の初期化
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# 保持されている会話履歴を表示
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Message to chatbot"):
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    
+    # チャットの実行
+    response = st.session_state.chain(prompt)
+    with st.chat_message("system"):
+        st.markdown(response["response"])
+    st.session_state.messages.append({"role": "assistant", "content": response["response"]})
